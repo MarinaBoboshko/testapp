25f77b6fa8b60d347b3bbbd079886bfa7882f67e
MarinaBoboshko <39676386+MarinaBoboshko@users.noreply.github.com>
2019-07-10 16:59:02 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,11 +2,7 @@ import random
 with open('text.txt', "w+") as f:
     for i in range(10):
         f.write(f"{random.randint(0, 10)}\n")
-print (i)
+with open(text.txt, 'r') as f:
+    print f.read()
 
 
-#b=[]
-#for i in range(10):
-#    a=random.randint(0, 100)
-#    b.append(a)
-#print(b)
